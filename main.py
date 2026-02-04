3a8f392bfbc456234622639d0d0d70b3180467b7
mocadayoo <192976698+mocadayoo@users.noreply.github.com>
2026-02-05 00:06:09 +0900

    add: fps counter & finger counter show on window

--- main.py
+++ main.py
@@ -48,67 +48,62 @@ def count_up_fingers(hand_landmarks, handedness):
         if hand_landmarks[tip].y < hand_landmarks[base].y:
             counter += 1
 
-    is_right_hand = handedness.category_name == "Right"
+    is_right = handedness.category_name == "Right"
     # 親指の付け根 < 小指の付け根で面裏を判断
-    is_palm = (hand_landmarks[5].x < hand_landmarks[17].x) if is_right_hand else (hand_landmarks[5].x > hand_landmarks[17].x)
+    is_palm = (hand_landmarks[5].x < hand_landmarks[17].x) if is_right else (hand_landmarks[5].x > hand_landmarks[17].x)
 
     # 親指は 2と4の点のxをさっきのpalmなどを使って条件を反転させながら判断
-    thumb_4 = hand_landmarks[4]
-    thumb_2 = hand_landmarks[2]
+    t4 = hand_landmarks[4]
+    t2 = hand_landmarks[2]
 
-    if is_right_hand:
-        if is_palm:
-            if thumb_4.x < thumb_2.x: counter += 1
-        else:
-            if thumb_4.x > thumb_2.x: counter += 1
-    else:
-        if is_palm:
-            if thumb_4.x > thumb_2.x: counter += 1
-        else:
-            if thumb_4.x < thumb_2.x: counter += 1
+    thumb_up = (t4.x < t2.x) if (is_right == is_palm) else (t4.x > t2.x)
+    if thumb_up: counter += 1
 
     return counter
 
-def draw_landmarks(image, detection_result):
-    if not detection_result.hand_landmarks:
-        return image # 手がない場合はただの画像のみ返す
-
-    h, w, _ = image.shape # カメラの画像の縦横比
-    for hand_landmarks in detection_result.hand_landmarks:
-        # 線を描画してから点を描画
-        for connect in HAND_CONNECTS:
-            p1 = hand_landmarks[connect[0]]
-            p2 = hand_landmarks[connect[1]]
-            cv.line(image,
-                    mp_convert_to_camera_wh(p1, w, h),
-                    mp_convert_to_camera_wh(p2, w, h),
-                    HAND_LINE_COLOR, HAND_LINE_TICKNESS)
-
-        for landmark in hand_landmarks:
-            cx, cy = mp_convert_to_camera_wh(landmark, w, h) # さっき取得した比率をもとに0~1を変換
-            cv.circle(image, (cx, cy), HAND_CIRCLE_RADIUS, HAND_CIRCLE_COLOR, HAND_CIRCLE_THICKNESS) # 緑の点で描画
-
-    return image
+def draw_landmarks(img, landmarks):
+    for line_point in HAND_CONNECTS:
+        cv.line(img, landmarks[line_point[0]], landmarks[line_point[1]], HAND_LINE_COLOR, HAND_LINE_TICKNESS)
+    for point in landmarks:
+        cv.circle(img, point, HAND_CIRCLE_RADIUS, HAND_CIRCLE_COLOR, HAND_CIRCLE_THICKNESS)
 
 camera = cv.VideoCapture(0)
+
+ret, first_frame = camera.read()
+H, W, _ = first_frame.shape
+
+SCALE_DOWN = 0.25
+resize = (int(W * SCALE_DOWN), int(H * SCALE_DOWN))
+
+prev_time = 0
 
 while camera.isOpened():
     ret, frame_bgr = camera.read()
     if not ret: break
 
     frame_bgr = cv.flip(frame_bgr, 1)
-    rgb_frame = cv.cvtColor(frame_bgr, cv.COLOR_BGR2RGB) # mediapipeのRBBに合わせる
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+    for_mp_frame = cv.resize(frame_bgr, resize, interpolation=cv.INTER_NEAREST)
+    rgb_mp_frame = cv.cvtColor(for_mp_frame, cv.COLOR_BGR2RGB) # mediapipeのRBBに合わせる
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_mp_frame)
 
-    timestamp_ms = int(time.time() * 1000)
+    current_time = time.time()
+    timestamp_ms = int(current_time * 1000)
     detection_result = detector.detect_for_video(mp_image, timestamp_ms)
 
-    for idx, hand_landmarks in enumerate(detection_result.hand_landmarks):
-        print(count_up_fingers(hand_landmarks, detection_result.handedness[idx][0]))
-    annotated_image = draw_landmarks(frame_bgr, detection_result)
+    if detection_result.hand_landmarks:
+        for idx, hand_landmarks in enumerate(detection_result.hand_landmarks):
+            cv.putText(frame_bgr, f"{count_up_fingers(hand_landmarks, detection_result.handedness[idx][0])}", ((idx * 30) + 150, 100), cv.FONT_HERSHEY_DUPLEX, 1, (0,0,0), 2)
 
-    cv.imshow('hand tracking test', annotated_image)
-    if cv.waitKey(1) & 0xFF == ord('l'):
+            scaleup_landmarks =  [(int(lm.x * W), int(lm.y * H)) for lm in hand_landmarks]
+            draw_landmarks(frame_bgr, scaleup_landmarks)
+
+    fps = 1 / (current_time - prev_time)
+    cv.putText(frame_bgr, f"FPS: {int(fps)}", (20, 50), cv.FONT_HERSHEY_DUPLEX, 1, (255,255,255), 2)
+
+    prev_time = current_time
+    cv.imshow('hand tracking test', frame_bgr)
+    ky = cv.waitKey(1)
+    if ky == ord("l"):
         break
 
 camera.release()
